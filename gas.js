9fb98adc7b6949fd7633684401eb4d88b84a6478
hkdnet <hkdnet@users.noreply.github.com>
2016-11-19 15:32:50 +0900

    バグってる

--- gas.js
+++ gas.js
@@ -1,5 +1,3 @@
-var token = "aMnZOrrfxp5BGBaOtPxYoSL+RQNeVljrA7s5CG3QGB320IXQiKOji+zGjOD97W6Dou9rebOQMKqDUxkJL/z4GGpvmOtAmPJetXpR4yuwHMM3V1C9yYYgZlQSq9fc7P7wo06uIlfjsXyVGWb1ovpvGgdB04t89/1O/w1cDnyilFU=";
-
 function doPost(e) {
   Logger.log(e);
   Logger.log("parse postData.contents");
@@ -8,30 +6,41 @@ function doPost(e) {
   Logger.log(json);
   var events = json.events;
   for(var i = 0, len = events.length; i < len; i++) {
-    return reply(events[i]);
+    var ev = events[i];
+    reply(ev);
+    // debug(ev.replyToken, e.postData);
   }
 }
 
+function debug(replyToken, postData) {
+  var messages = [
+    { type: "text", text: JSON.stringify(postData) },
+  ];
+  sendReply(replyToken, messages);
+}
+
 function reply(e){
+  var messages = [
+    { type: "text", text: "Always hello" },
+  ];
+  sendReply(e.replyToken, messages);
+}
+
+function sendReply(replyToken, messages) {
   var url = 'https://api.line.me/v2/bot/message/reply';
+  var token = "aMnZOrrfxp5BGBaOtPxYoSL+RQNeVljrA7s5CG3QGB320IXQiKOji+zGjOD97W6Dou9rebOQMKqDUxkJL/z4GGpvmOtAmPJetXpR4yuwHMM3V1C9yYYgZlQSq9fc7P7wo06uIlfjsXyVGWb1ovpvGgdB04t89/1O/w1cDnyilFU=";
   var headers = {
     "Content-Type" : "application/json; charset=UTF-8",
     "Authorization": "Bearer " + token,
   };
   var data = {
-    "replyToken": e.replyToken,
-    "messages": [
-      { type: "text", text: "Always hello" },
-    ]
+    "replyToken": replyToken,
+    "messages": messages
   };
-
-
   var options = {
     "method" : "post",
     "headers" : headers,
     "payload" : JSON.stringify(data)
   };
-
   return UrlFetchApp.fetch(url, options);
 }
-
